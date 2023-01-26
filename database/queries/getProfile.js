d21032a9bd14d0852a7a65b93af7fa6bd1104ad3
Chuck Song <chuckthedeveloper@gmail.com>
2023-01-25 23:44:29 -0500

    Completed getProfile router

---
+++ database/queries/getProfile.js
@@ -0,0 +1,57 @@
+const pool = require('../index.js');
+
+module.exports = (user_id) => {
+
+  const query = {
+    text: `
+    SELECT json_agg(user_profile) FROM (
+      SELECT user_id, username, first_name, last_name, email, created_time, (
+          SELECT coalesce(json_agg(photos), '[]'::json) FROM (
+            SELECT photo_id, photo_url FROM profile_photos WHERE user_id = public.user.user_id) photos
+        ) AS photos, (
+          SELECT coalesce(json_agg(fav_games), '[]'::json) FROM (
+          SELECT game_id FROM favgames WHERE user_id = public.user.user_id) fav_games
+      ) AS fav_games, (
+        SELECT coalesce(json_agg(friends_list), '[]'::json) FROM (
+          SELECT user2_id AS userid FROM public.friend_relation
+          WHERE user1_id=public.user.user_id
+          AND user1_req_user2='approved'
+          AND NOT user1_blk_user2=true) friends_list
+    ) AS friends, (
+      SELECT coalesce(json_agg(blocked_list), '[]'::json) FROM (
+        SELECT user2_id AS userid FROM public.friend_relation
+        WHERE user1_id=public.user.user_id
+        AND user1_blk_user2=true) blocked_list
+  ) AS blocked_users, (
+    SELECT coalesce(json_agg(pending_req), '[]'::json) FROM (
+      SELECT user1_id AS userid FROM public.friend_relation
+      WHERE user2_id=public.user.user_id
+      AND user1_req_user2='pending') pending_req
+) AS received_req_from
+
+      FROM public.user
+      WHERE user_id=${user_id}
+      ORDER BY created_time DESC
+      ) user_profile;
+    ;`
+  }
+
+  return pool
+    .connect()
+    .then(client => {
+      return client
+        .query(query)
+        .then(async res => {
+          const response = {};
+          response['users'] = res.rows[0].json_agg;
+          client.release()
+          return response
+        })
+        .catch(err => {
+          client.release()
+          console.log(err.stack)
+          return err.stack
+        })
+    })
+    .catch(err => {console.log(err)})
+}
--- server/router.js
+++ server/router.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const models = require('./models.js');
 
 router.get('/users', models.getUsers);
+router.get('/user/:user_id/profile', models.getProfile);
 
 module.exports = router;
