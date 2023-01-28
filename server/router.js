ac4fed0039da664f1a8ed1954e21b9aa8bb5436d
Chuck Song <chuckthedeveloper@gmail.com>
2023-01-27 21:24:18 -0500

    Completed like and unlike router

--- server/router.js
+++ server/router.js
@@ -7,5 +7,6 @@ router.get('/user/:user_id/profile', models.getProfile);
 router.post('/friends/:user1_id/request/:user2_id', models.addFriend);
 router.post('/friends/:user1_id/respond/:user2_id', models.respondFriend);
 router.post('/friends/:user1_id/block/:user2_id', models.blockedFriend);
+router.post('/game/:user_id/:game_id', models.likeGame);
 
 module.exports = router;
